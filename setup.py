543a2f1ee90232e44ea4a3d59e5292a90befa39d
Midhun C Nair <midhunch@gmail.com>
2020-03-13 21:21:16 +0530

    added the check command for twine

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --user --upgrade twine
+python3 -m twine check dist/*  # fix the issue if any
 python3 -m twine upload dist/*
 """
 
